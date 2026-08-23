48be1409c77a6834208e0137d1397d24375ab858
Himanshu chahar <himanshuchahar6396@gmail.com>
2026-08-24 03:44:34 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = [
+    "django-project-fullstack-dev.vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
-ALLOWED_HOSTS = ['.vercel.app']
 
 
 
